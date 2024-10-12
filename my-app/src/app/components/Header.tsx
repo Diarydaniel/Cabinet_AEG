5960081f089fd6457a64bb6cb2200d5b03415049
Daniel <razdaniel25@gmail.com>
2024-10-12 17:11:32 +0300

    Partier Header 50%

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+'use client';
+
+import React, { useState, useEffect } from 'react'
 import './header.css'
 
 export default function Header() {
+    //Déclaration des variables à utiliser
+    const [scroll, setScroll] = useState(0);
+
+    //Récupération du scroll en temps réel
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setScroll(window.scrollY);
+        });
+        return () => {
+            window.removeEventListener('scroll', () => {
+                setScroll(window.scrollY);
+            });
+        };
+    }, [scroll]);
+
     return (
-        <div>
-            Header
-        </div>
+        <header
+            id='header'
+            className={`d-flex align-items-center fixed-top ${scroll > 100 ? 'header-scrolled' : undefined
+
+                }`}
+        >
+            <div className="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+                <h1 className="logo me-auto me-lg-0">
+                    <a href="/">CABINET AEG</a>
+                </h1>
+                {/* <a href="index.html" className="logo me-auto me-lg-0">
+                    <img src="assets/img/logo.png" alt="" className='img-fluid' />
+                </a> */}
+            </div>
+        </header>
     )
 }
